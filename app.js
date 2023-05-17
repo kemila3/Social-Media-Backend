0f55363668a1252cd876e2a5b953fadeac515958
Kemila <kemiladabare9@gmail.com>
2023-05-17 22:24:52 +0530

    localhost working? experimented

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-const express = require("express")
+import express from "express"
+
+const app = express();
+
+app.use('/', (req,res,next) => {
+    res.send("Hello world how u doin hehe")
+})
+
+app.listen(5000)
