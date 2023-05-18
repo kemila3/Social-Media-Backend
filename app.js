0df3a114621f2aea2a5617c0019ee072a96d2587
Kemila <kemiladabare9@gmail.com>
2023-05-18 12:51:44 +0530

    user functionality done

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/userRoutes";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.use("/api/user", router);
 mongoose
-  .connect(
-    "mongodb+srv://admin:admin@cluster0.rs4hnjp.mongodb.net/Blog?retryWrites=true&w=majority"
-  )
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => app.listen(5000))
   .then(() => console.log("MongoDB is connected!"))
   .catch((err) => console.log(err));
-
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 import User from "../model/userModel";
+import bcrypt from "bcryptjs";
 
+//GET method to get all the users
 export const getAllUsers = async (req, res, next) => {
   let users;
   try {
@@ -14,6 +16,7 @@ export const getAllUsers = async (req, res, next) => {
   return res.status(200).json({ users });
 };
 
+// signup a new user using POST method
 export const signup = async (req, res, next) => {
   const { name, email, password } = req.body;
 
@@ -28,10 +31,11 @@ export const signup = async (req, res, next) => {
       .status(400)
       .json({ message: "User already exists..Login instead!" });
   }
+  const hashedPassword = bcrypt.hashSync(password);
   const user = new User({
     name,
     email,
-    password,
+    password: hashedPassword,
   });
 
   try {
@@ -39,5 +43,27 @@ export const signup = async (req, res, next) => {
   } catch (err) {
     console.log(err);
   }
-  return res.status(201).json({user})
+  return res.status(201).json({ user });
+};
+
+// POST login
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  let exisitngUser;
+  try {
+    exisitngUser = await User.findOne({ email });
+  } catch (err) {
+    console.log(err);
+  }
+  if (!exisitngUser) {
+    return res
+      .status(404)
+      .json({ message: "Couldn't find user by this email!" });
+  }
+
+  const isPasswordCorrect = bcrypt.compareSync(password, exisitngUser.password)
+  if(!isPasswordCorrect){
+    return res.status(400).json({message: "Incorrect password"})
+  }
+  return res.status(200).json({message: "Login Successful"})
 };
